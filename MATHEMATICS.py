af2ec8403fb542553acefaff53c510038ce62fe9
S-VANSH <166596167+S-VANSH@users.noreply.github.com>
2024-04-17 22:33:30 +1200

    Update MATHEMATICS.py

--- MATHEMATICS.py
+++ MATHEMATICS.py
@@ -1,7 +1,18 @@
-#person
-class Person:
-    def __init__(self,name):
-        self.name = name
-        
-    def greet(self):
-        print(f"Hello,{self.name}!")
+a = 15
+b = 10
+c = a + b
+#addition
+print("addition of two numbers:",c)
+d = a - b
+#substaction
+print("substraction of two numbers:",d)
+e = a * b
+#multiplication
+print("multiplication of two numbers:",e)
+f = a / b
+#divion
+print("division of two numbers:",f)
+g = a % b
+#module
+print("module of two numbers:",g)
+h = a ** b
