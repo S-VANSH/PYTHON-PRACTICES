427b5ef10694432a63930c2a69de3fe869fa3f6f
S-VANSH <166596167+S-VANSH@users.noreply.github.com>
2024-04-17 22:01:51 +1200

    Update FUNCTION.py

--- FUNCTION.py
+++ FUNCTION.py
@@ -1,46 +1,6 @@
-def displayname():
-    first_name = input("write your first name:")
-    second_name = input("write your second name:")
-    
-    full_name = first_name + second_name
-    
-    print(full_name)
-    
-    displayname()
-
-
-
-
-
-
-
-
-# def sum():
-#     a = int(input("enter the fisrt number"))
-#     b = int(input("enter the second number"))
-#     print(a + b)
-    
-    
-# sum()
-
-
-
-
-
-# def main():
-#     radius = float(input("enter the radius of the circle: "))
-#     area = calculate_area
-#     print(area)
-    
-# main()
-
-
-# leap year 
-# def leap_year():
-#     if(year % 4 == 0 and year % 100 ! = 0) or (year % 400 ==)
-#     return True
-# else:return False
-    
-    
-               
-    
+def sum():
+    a = int(input("enter the fisrt number"))
+    b = int(input("enter the second number"))
+    print(a + b)
+        
+sum()
