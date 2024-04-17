6e7fba53d372e24a79bd59290986d4ff57a38be0
S-VANSH <166596167+S-VANSH@users.noreply.github.com>
2024-04-17 22:16:28 +1200

    Add files via upload

---
+++ LIST.py
@@ -0,0 +1,42 @@
+# def new_group(group_name, members):
+#     group_name = "eagles"
+#     members = "sahil", "vansh", "harshdeep"
+    
+# new_group= ("harsh", "sahil")
+# print(new_group)
+# new_group()
+
+
+# def generate_band_name():
+#     print("welcome top the band name generate.")
+#     city = input("which city did you grow up in? \n")
+#     pet = input("what is the name if you have a pet? \n")
+#     return f"your band name could be(city.capitalize())(pet.captilaize())"
+
+# band_name = generate_band_name()
+# print(band_name)
+
+
+# # from class
+# def make_group(person1, person2):
+#     group = [person1, person2]
+#     return group
+
+# #make_group = lamda person1, person2: [person1, person2]
+
+# #create object
+# student1 = "james bond"
+# student2 = "batman"
+
+# #calling the function
+
+# print(f"group 1:{make_group(student1, student2)}")   
+# group2 = make_group("somebody", "anotherbody")
+# print(f"group 2: {group2}")
+
+#list
+shopping_cart = ["kiwi", "banana", "apple", "orange"]
+shopping_cart.append("chilli")
+print(shopping_cart)
+shopping_cart.remove("banana")
+print(shopping_cart)
